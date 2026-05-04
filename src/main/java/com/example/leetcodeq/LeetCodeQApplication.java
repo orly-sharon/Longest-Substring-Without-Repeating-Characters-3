971af299e419f0428724aa0091fa854415f7dfff
Orly Sharon <orlyswork@gmail.com>
2026-05-04 12:03:15 +0300

    add nevagito bar back nad new open screen

--- src/main/java/com/example/leetcodeq/LeetCodeQApplication.java
+++ src/main/java/com/example/leetcodeq/LeetCodeQApplication.java
@@ -4,7 +4,40 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.HashMap;
+/*
+3. Longest Substring Without Repeating Characters
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given a string s, find the length of the longest substring without duplicate characters.
 
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+
+0 <= s.length <= 5 * 104
+ */
 @SpringBootApplication
 public class LeetCodeQApplication {
 
